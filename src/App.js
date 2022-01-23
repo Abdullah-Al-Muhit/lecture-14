bb6f3ec016c046062b90ccdf39b6fcbca484189b
MuhitMridul <muhitmridul71@gmail.com>
2022-01-24 00:29:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import ClickCounter from './components/ClickCounter';
+import Counter from './components/Counter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Counter>
+                {(counter, incrementCount) => (
+                    <ClickCounter count={counter} incrementCount={incrementCount} />
+                )}
+            </Counter>
+            <Counter>
+                {(counter, incrementCount) => (
+                    <HoverCounter count={counter} incrementCount={incrementCount} />
+                )}
+            </Counter>
+        </div>
+    );
 }
 
 export default App;
